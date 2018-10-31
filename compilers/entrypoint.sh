251dc463f9948ddddcb594dc908afcf183b7d7fe
Anil Madhavapeddy <anil@recoil.org>
2018-10-31 21:56:55 +0000

    switches
    Signed-off-by: Anil Madhavapeddy <anil@recoil.org>

--- compilers/entrypoint.sh
+++ compilers/entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh -ex
 
+if [ "${OCAML_VERSION}" != "" ]; then
+  OPAMSWITCH="${OCAML_VERSION}"
+fi
+
 if [ "$*" = "" ]; then
   LOCAL_PKGS=`ls *.opam|xargs -I'{}' basename '{}' .opam`
-  opam init -ya --disable-sandboxing
   opam depext -u odoc ${LOCAL_PKGS}
   # TODO get a host binary of odoc instead
   opam install -y odoc
