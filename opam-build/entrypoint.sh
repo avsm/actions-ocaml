7f9642bf708a482a0c3198f2ea6710fd91ed2c32
Anil Madhavapeddy <anil@recoil.org>
2018-10-31 20:19:47 +0000

    upda

--- opam-build/entrypoint.sh
+++ opam-build/entrypoint.sh
@@ -2,7 +2,7 @@
 
 LOCAL_PKGS=`ls *.opam|xargs -I'{}' basename '{}' .opam`
 opam init -ya --disable-sandboxing
-# TODO get a host binary instead
+opam depext -u odoc ${LOCAL_PKGS}
+# TODO get a host binary of odoc instead
 opam install -y odoc
-opam depext -ui ${LOCAL_PKGS}
 opam install -y --with-test .
