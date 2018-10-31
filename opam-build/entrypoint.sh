fd6ae05ac7d24361de7c11f0089933c2d22f95c6
Anil Madhavapeddy <anil@recoil.org>
2018-10-31 19:57:13 +0000

    improve

--- opam-build/entrypoint.sh
+++ opam-build/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -ex
 
+LOCAL_PKGS=`ls *.opam|xargs -I'{}' basename '{}' .opam`
 opam init -ya --disable-sandboxing
 opam install -y odoc
 opam pin add -n .
-opam depext -ui $*
-opam install -ty $*
+opam depext -ui ${LOCAL_PKGS}
+opam install -y --with-test .
