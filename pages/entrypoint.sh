c394cc66974e155437de5d8fce50f05b8405b16a
Anil Madhavapeddy <anil@recoil.org>
2018-10-31 18:03:07 +0000

    pages

--- pages/entrypoint.sh
+++ pages/entrypoint.sh
@@ -1,18 +1,18 @@
 #!/bin/sh -ex
 
-rm -rf /tmp/docs
-mkdir /tmp/docs
-rsync -av $GITHUB_WORKSPACE/.git /tmp/docs/.git
-
 git config credential.helper 'cache --timeout=120'
 git config user.email "anil@recoil.org"
 git config user.name "Github Action"
 
-git -C /tmp/docs checkout --orphan gh-pages
-git -C /tmp/docs reset
-git -C /tmp/docs clean -dxf
-cp -r $GITHUB_WORKSPACE/_build/default/_doc/_html/* /tmp/docs/
-git -C /tmp/docs add .
-git -C /tmp/docs commit -m "Update Pages"
-git -C /tmp/docs push -q https://${GITHUB_PERSONAL_TOKEN}@github.com/${GITHUB_REPOSITORY}.git gh-pages -f
 rm -rf /tmp/docs
+mkdir /tmp/docs
+cd /tmp/docs
+git init
+git checkout --orphan gh-pages
+git reset
+git clean -dxf
+#cp -r $GITHUB_WORKSPACE/_build/default/_doc/_html/* /tmp/docs/
+cp -r $GITHUB_WORKSPACE/*.opam /tmp/docs/
+git add .
+git commit -m "Update Pages"
+git push -q https://${GITHUB_PERSONAL_TOKEN}@github.com/${GITHUB_REPOSITORY}.git gh-pages -f
