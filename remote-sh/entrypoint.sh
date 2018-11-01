85d4e514d10d9a7f112fdd6b9ddb12b4c25b5861
Anil Madhavapeddy <anil@recoil.org>
2018-11-01 19:28:19 +0000

    fix keyscan

--- remote-sh/entrypoint.sh
+++ remote-sh/entrypoint.sh
@@ -5,6 +5,10 @@ if [ "$HOST" = "" ]; then
   exit 1
 fi
 
+if [ "$HOST_USER" = "" ]; then
+  HOST_USER="root"
+fi
+
 if [ "$HOST_SSH_KEY" = "" ]; then
    echo Must specify HOST_SSH_KEY in secrets configuration.
 fi
@@ -13,9 +17,9 @@ fi
 UUID=`uuidgen -r`
 
 mkdir -p ~/.ssh
-ssh-keyscan >> ~/.ssh/known_hosts
+ssh-keyscan $HOST >> ~/.ssh/known_hosts
 echo "$HOST_SSH_KEY" > ~/.ssh/id_rsa
 chmod -R 600 ~/.ssh 
 
-ssh $HOST ls -la
-ssh $HOST id
+ssh "$HOST_USER@$HOST" ls -la
+ssh "$HOST_USER@$HOST" id
