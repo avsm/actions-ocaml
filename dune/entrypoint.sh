92fdde5b09dc3111bfcb495bc55db06223b0784f
Anil Madhavapeddy <anil@recoil.org>
2018-11-01 10:17:33 +0000

    more
    Signed-off-by: Anil Madhavapeddy <anil@recoil.org>

--- dune/entrypoint.sh
+++ dune/entrypoint.sh
@@ -1,3 +1,15 @@
 #!/bin/sh -ex
 
+# Ensure we persist the opam root
+if [ -d "${GITHUB_WORKSPACE}/.opam" ]; then
+  mv /home/opam/.opam /home/opam/.opam.old
+else
+  mv /home/opam/.opam ${GITHUB_WORKSPACE}/.opam
+fi
+ln -s ${GITHUB_WORKSPACE}/.opam /home/opam/.opam
+
+if [ "${OCAML_VERSION}" != "" ]; then
+  OPAMSWITCH="${OCAML_VERSION}"
+fi
+
 sh -c "opam config exec -- dune $* --verbose"
