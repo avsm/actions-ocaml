d3677afaf19bc7ebd81799f35312902f5991fb56
Anil Madhavapeddy <anil@recoil.org>
2018-11-01 21:34:47 +0000

    sync workspaces

--- remote-sh/entrypoint.sh
+++ remote-sh/entrypoint.sh
@@ -18,21 +18,23 @@ chmod 700 /root/.ssh
 echo "$HOST_SSH_KEY" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
 
-set -x
-
 # Construct a fresh directory on remote host
 UUID=`uuidgen -r`
 
 SSH="ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=no -l$HOST_USER $HOST"
 
 function cleanup {
+  echo Transferring workspace/ back to $GITHUB_WORKSPACE locally.
+  rsync -avz ${HOST_USER}@${HOST}:${UUID}/ ${GITHUB_WORKSPACE}/
   echo Cleaning up $UUID on remote server.
   $SSH rm -rf $UUID
 }
 
 trap cleanup EXIT
 
-$SSH mkdir $UUID
+$SSH mkdir $UUID/workspace
+echo Syncing workspace to remote server with uuid ${UUID}
+rsync -avz ${GITHUB_WORKSPACE}/ ${HOST_USER}@${HOST}:${UUID}/
 
 for cmd in "$@"; do
     echo "Running '$cmd' on remote server"
