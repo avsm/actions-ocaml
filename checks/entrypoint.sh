f8f1b30aab45bc8fda04704098bc086c89f14ba3
Anil Madhavapeddy <anil@recoil.org>
2018-11-05 20:25:54 +0000

    fix curl
    Signed-off-by: Anil Madhavapeddy <anil@recoil.org>

--- checks/entrypoint.sh
+++ checks/entrypoint.sh
@@ -5,4 +5,4 @@ cat /github/workflow/event.json
 
 PAYLOAD=$(echo '{}' | jq ".name = \"testcheck\" | .head_sha=\"$GITHUB_SHA\"")
 
-curl -X POST -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --header "Accept: application/vnd.github.antiope-preview+json" --data="$PAYLOAD" https://api.github.com/avsm/actions-test/check-runs 
+curl -X POST -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --header "Accept: application/vnd.github.antiope-preview+json" --data "$PAYLOAD" https://api.github.com/avsm/actions-test/check-runs 
