fed0a81b5459b2a02bd3f0d5044e30fc37df922d
Anil Madhavapeddy <anil@recoil.org>
2018-11-05 21:17:12 +0000

    more
    Signed-off-by: Anil Madhavapeddy <anil@recoil.org>

--- checks/entrypoint.sh
+++ checks/entrypoint.sh
@@ -2,9 +2,18 @@
 
 jq . /github/workflow/event.json
 
+curl -S \
+  -H "Authorization: token ${GITHUB_TOKEN}" \
+  -H "Content-Type: application/json" \
+  "https://api.github.com/repos/${GITHUB_REPOSITORY}/commits/${GITHUB_SHA}/check-runs?check_name=${GITHUB_ACTION}"
+
 PAYLOAD=`echo '{}' | jq ".name = \"checks\" | .head_sha=\"$GITHUB_SHA\" | .output.title=\"checkrunresult\" | .output.summary=\"summary here\" | .output.text=\"longer text here\n### test headings\nand some more *bold*\""`
+
 CHECK_RUN_URL=`cat /github/workflow/event.json | jq -r .url`
+
+
 curl -X POST -S \
   -H "Authorization: token ${GITHUB_TOKEN}" \
   -H "Content-Type: application/json" \
-  -H "Accept: application/vnd.github.antiope-preview+json" --data "$PAYLOAD"  ${CHECK_RUN_URL}
+  -H "Accept: application/vnd.github.antiope-preview+json" \
+  --data "$PAYLOAD"  ${CHECK_RUN_URL}
