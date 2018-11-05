7d6350859679c3b1846d505a990aa4fbcadfffe0
Anil Madhavapeddy <anil@recoil.org>
2018-11-05 20:37:12 +0000

    fix url
    Signed-off-by: Anil Madhavapeddy <anil@recoil.org>

--- checks/entrypoint.sh
+++ checks/entrypoint.sh
@@ -6,4 +6,4 @@ env
 
 PAYLOAD=$(echo '{}' | jq ".name = \"testcheck\" | .head_sha=\"$GITHUB_SHA\"")
 
-curl -X POST -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --header "Accept: application/vnd.github.antiope-preview+json" --data "$PAYLOAD" https://api.github.com/avsm/actions-test/check-runs 
+curl -X POST -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --header "Accept: application/vnd.github.antiope-preview+json" --data "$PAYLOAD" https://api.github.com/repos/avsm/actions-test/check-runs 
