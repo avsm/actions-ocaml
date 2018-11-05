0c0319a42bf5e6bfaac3e3e18cf95f59aa79672e
Anil Madhavapeddy <anil@recoil.org>
2018-11-05 20:30:45 +0000

    debug
    Signed-off-by: Anil Madhavapeddy <anil@recoil.org>

--- checks/entrypoint.sh
+++ checks/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -ex
 
+ls -la /github/workflow
 cat /github/workflow/event.json
-
+env
 
 PAYLOAD=$(echo '{}' | jq ".name = \"testcheck\" | .head_sha=\"$GITHUB_SHA\"")
 
-curl -X POST -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --header "Accept: application/vnd.github.antiope-preview+json" --data "$PAYLOAD" https://api.github.com/avsm/actions-test/check-runs 
+curl -X POST -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --header "Accept: application/vnd.github.antiope-preview+json" --data "$PAYLOAD" https://api.github.com/avsm/actions-test/check-runs 
