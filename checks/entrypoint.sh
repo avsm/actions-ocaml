28ca19f02db95286bbf8766eb73a86ab5b2f4fad
Anil Madhavapeddy <anil@recoil.org>
2018-11-06 09:01:40 +0000

    more
    Signed-off-by: Anil Madhavapeddy <anil@recoil.org>

--- checks/entrypoint.sh
+++ checks/entrypoint.sh
@@ -9,7 +9,7 @@ RUN_INFO=`curl -S \
   "https://api.github.com/repos/${GITHUB_REPOSITORY}/commits/${GITHUB_SHA}/check-runs?check_name=${GITHUB_ACTION}"`
 CHECK_RUN_URL=`echo "${RUN_INFO}" | jq -r '.check_runs[0].url'`
 
-PAYLOAD=`echo '{}' | jq ".name = \"checks\" | .head_sha=\"$GITHUB_SHA\" | .output.title=\"checkrunresult\" | .output.summary=\"summary here\" | .output.text=\"longer text here\n### test headings\nand some more *bold*\" | .actions.label=\"foo\" | .actions.description=\"does stuff\" | .actions.identifier=\"bar\""`
+PAYLOAD=`echo '{}' | jq ".name = \"checks\" | .head_sha=\"$GITHUB_SHA\" | .output.title=\"checkrunresult\" | .output.summary=\"summary here\" | .output.text=\"longer text here\n### test headings\nand some more *bold*\" | .actions[0].label=\"foo\" | .actions[0].description=\"does stuff\" | .actions[0].identifier=\"bar\" | .annotations[0].path=\"README.md\" |.annotations[0].start_line=\"2\" | .annotations[0].end_line=\"5\" |.annotations[0].annotation_level=\"warning\" | .annotations[0].message=\"hello world\" | .annotations[0].title=\"testinggg\""`
 
 curl -X PATCH -S \
   -H "Authorization: token ${GITHUB_TOKEN}" \
