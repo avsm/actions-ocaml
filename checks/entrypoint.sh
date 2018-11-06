962d980d738e1b6ab2d2e07aab918e011fd9704b
Anil Madhavapeddy <anil@recoil.org>
2018-11-06 08:48:34 +0000

    fix
    Signed-off-by: Anil Madhavapeddy <anil@recoil.org>

--- checks/entrypoint.sh
+++ checks/entrypoint.sh
@@ -7,7 +7,7 @@ RUN_INFO=`curl -S \
   -H "Content-Type: application/json" \
   -H "Accept: application/vnd.github.antiope-preview+json" \
   "https://api.github.com/repos/${GITHUB_REPOSITORY}/commits/${GITHUB_SHA}/check-runs?check_name=${GITHUB_ACTION}"`
-CHECK_RUN_URL=`cat ${GITHUB_EVENT_PATH} | jq -r '.check_runs[0].url'`
+CHECK_RUN_URL=`echo "${RUN_INFO}" | jq -r '.check_runs[0].url'`
 
 PAYLOAD=`echo '{}' | jq ".name = \"checks\" | .head_sha=\"$GITHUB_SHA\" | .output.title=\"checkrunresult\" | .output.summary=\"summary here\" | .output.text=\"longer text here\n### test headings\nand some more *bold*\""`
 
