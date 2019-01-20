020534c100c1dd07a676daa05fa1260057d8bb8e
Anil Madhavapeddy <anil@recoil.org>
2019-01-20 20:29:12 +0000

    more

--- docker-test/entrypoint.sh
+++ docker-test/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -ex
 
-docker run ${DOCKER_IMAGE}:master opam list
+docker run -v ${GITHUB_WORKSPACE}:/mnt ${DOCKER_IMAGE}:master opam list 
+docker run -v ${GITHUB_WORKSPACE}:/mnt ${DOCKER_IMAGE}:master ls -la /mnt/
